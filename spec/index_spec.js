5d49aa35fcb51136d6ae6ddb06109c34aebae937
Jason Mavandi <Jason.Mavandi@emc.com>
2016-04-08 19:24:38 -0600

    Fix readme
    Add another test

--- spec/index_spec.js
+++ spec/index_spec.js
@@ -1,10 +1,18 @@
 var TestRail = require("../lib"), creds = require("../credentials.json");
 var tr = new TestRail(creds.url,creds.username,creds.password);
+tr.allowUntrustedCertificate();
 
 describe("index", function() {
 
     it("getFullApiPath", function() {
         expect(tr.getFullApiPath()).toBe(creds.url + "/index.php?/api/v2/");
     });
-    
+
+    it("getStatuses", function(done) {
+        tr.getStatuses().then(function(statuses){
+            expect(statuses[0].name).toBe("passed");
+            done();
+        });
+    });
+
 });
